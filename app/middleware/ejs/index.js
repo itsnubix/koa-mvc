a6b80f155f63c8e4892d6f14065e060d38152c04
Jim Geurts <jim@biacreations.com>
2017-03-06 22:49:23 -0600

    Store base path of app on KoaConfig object

--- app/middleware/ejs/index.js
+++ app/middleware/ejs/index.js
@@ -4,14 +4,14 @@ const render = require('koa-ejs');
 const path = require('path');
 
 module.exports = (app) => {
-  const viewPath = path.join(__dirname, '../../../views');
+  const viewPath = path.join(KoaConfig.path, 'views');
   render(app, {
     root: viewPath,
     viewExt: 'ejs',
   });
 
   // The work of this middleware was done above with the render function, so just return a blank
-  // method to satisfy the koa middleware needs
+  // method to satisfy the per request middleware needs
   return async (context, next) => {
     await next();
   };
--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const path = require('path');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 const environmentConfig = require('./env');
@@ -12,6 +13,7 @@ const session = require('./session');
 const environmentOverrides = environmentConfig[nodeEnv] || {};
 
 module.exports = _.merge({
+  path: path.join(__dirname, '..'),
   log,
   policies,
   routes,
