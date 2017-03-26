f8b85124c9589ff6f70c9c2718452fef5f6feda9
Jim Geurts <jim@biacreations.com>
2017-03-26 11:10:44 -0500

    Allow flash function to be accessed by state

--- app/middleware/flash/index.js
+++ app/middleware/flash/index.js
@@ -4,24 +4,29 @@ const _ = require('lodash');
 
 module.exports = () => {
   return async function flash(context, next) {
-    context.state.flash = context.session.flash || {};
-    context.flash = function saveFlash(severity, message) {
-      context._flash = context._flash || {};
-      context._flash[severity] = context._flash[severity] || [];
-
-      if (_.isArray(message)) {
-        context._flash[severity].push(...message);
+    function flash(severity, message) {
+      if (severity && message) {
+        context.session.flash[severity] = context.session.flash[severity] || [];
+        if (_.isArray(message)) {
+          context.session.flash[severity].push(...message);
+        } else {
+          context.session.flash[severity].push(message);
+        }
+      } else if (severity) {
+        const messages = context.session.flash[severity] || [];
+        delete context.session.flash[severity];
+        return messages;
       } else {
-        context._flash[severity].push(message);
+        const messages = context.session.flash;
+        context.session.flash = {};
+        return messages;
       }
-    };
+    }
+
+    context.session.flash = context.session.flash || {};
+    context.flash = flash;
+    context.state.flash = flash;
 
     await next();
-
-    if (_.isEmpty(context._flash)) {
-      delete context.session.flash;
-    } else {
-      context.session.flash = context._flash;
-    }
   };
 };
