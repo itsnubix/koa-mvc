20b4b0fe67e72f0bb3cf3ac32941d2e8c20d5e35
Jim Geurts <jim@biacreations.com>
2017-04-02 09:51:53 -0500

    Separate app for easier testing

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 'use strict';
 
-const startupTime = process.hrtime();
-
-const Koa = require('koa');
-const config = require('./config');
-
-global.KoaConfig = config;
-
+const app = require('./app');
 const logService = require('./app/services/logService');
-const metricService = require('./app/services/metricService');
-const middleware = require('./app/middleware');
 
-(async function loadApplication() {
-  const app = new Koa();
-  await middleware.plugins.startup(app);
-  app.use(middleware.middleware());
-  app.listen(config.port);
-
-  process.on('SIGINT', () => {
-    (async function shutdown() {
-      await middleware.plugins.shutdown();
-
-      process.exit();
-    }()).catch((ex) => {
-      logService.error(ex);
-      process.exit();
-    })
-  });
-
-  logService.info(`Listening on port ${config.port}`);
-  metricService.duration('application.startupTime', startupTime);
-}()).catch((ex) => {
+app.load().catch((ex) => {
   logService.error(ex);
   throw ex;
 });
