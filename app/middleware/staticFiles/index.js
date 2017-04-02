6d03e7dcee949fef36e17f21c9bf40b4e20b5068
Jim Geurts <jim@biacreations.com>
2017-04-02 09:56:26 -0500

    Add cache age for production static files

--- app/middleware/staticFiles/index.js
+++ app/middleware/staticFiles/index.js
@@ -6,6 +6,7 @@ module.exports = () => {
   return async function staticFiles(context) {
     await send(context, context.path, {
       root: `${KoaConfig.path}/.tmp`,
+      maxage: KoaConfig.environment === 'production' ? 31557600000 : 0,
     });
   };
 };
