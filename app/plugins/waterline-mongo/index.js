d0d72ded26d953ce230863fe5868609a5b9d52bd
Jim Geurts <jim@biacreations.com>
2017-04-13 10:30:06 -0500

    Handle case when no models are defined

--- app/plugins/waterline-mongo/index.js
+++ app/plugins/waterline-mongo/index.js
@@ -5,55 +5,60 @@ const fs = require('mz/fs');
 const path = require('path');
 const Waterline = require('waterline');
 const sailsMongo = require('sails-mongo2');
+const logService = require('../../services/logService');
 
 module.exports = {
   startup: async function waterlineMongoPluginStartup() {
-    if (KoaConfig.datastores) {
-      const waterline = new Waterline();
+    try {
+      if (KoaConfig.datastores) {
+        const waterline = new Waterline();
 
-      const modelsPath = path.join(KoaConfig.path, '/app/models');
-      const files = await fs.readdir(modelsPath);
+        const modelsPath = path.join(KoaConfig.path, '/app/models');
+        const files = await fs.readdir(modelsPath);
 
-      for (const file of files) {
-        if (/.js$/ig.test(file)) {
-          const fileBasename = path.basename(file, '.js');
-          /* eslint-disable global-require, import/no-dynamic-require */
-          const schema = require(`${modelsPath}/${fileBasename}`);
-          /* eslint-enable global-require, import/no-dynamic-require */
+        for (const file of files) {
+          if (/.js$/ig.test(file)) {
+            const fileBasename = path.basename(file, '.js');
+            /* eslint-disable global-require, import/no-dynamic-require */
+            const schema = require(`${modelsPath}/${fileBasename}`);
+            /* eslint-enable global-require, import/no-dynamic-require */
 
-          const model = _.merge({
-            identity: fileBasename.toLowerCase(),
-            globalId: fileBasename,
-            tableName: fileBasename.toLowerCase(),
-            connection: 'default',
-            migrate: 'alter',
-            dynamicFinders: false,
-          }, schema);
+            const model = _.merge({
+              identity: fileBasename.toLowerCase(),
+              globalId: fileBasename,
+              tableName: fileBasename.toLowerCase(),
+              connection: 'default',
+              migrate: 'alter',
+              dynamicFinders: false,
+            }, schema);
 
-          waterline.loadCollection(Waterline.Collection.extend(model));
-        }
-      }
-
-      const waterlineConfig = {
-        adapters: {
-          'sails-mongo2': sailsMongo,
-        },
-        connections: KoaConfig.datastores,
-      };
-
-      await new Promise((resolve, reject) => {
-        waterline.initialize(waterlineConfig, (err, orm) => {
-          if (err) {
-            return reject(err);
+            waterline.loadCollection(Waterline.Collection.extend(model));
           }
+        }
 
-          _.each(orm.collections, (model) => {
-            global[model.globalId || 'unknown'] = model;
+        const waterlineConfig = {
+          adapters: {
+            'sails-mongo2': sailsMongo,
+          },
+          connections: KoaConfig.datastores,
+        };
+
+        await new Promise((resolve, reject) => {
+          waterline.initialize(waterlineConfig, (err, orm) => {
+            if (err) {
+              return reject(err);
+            }
+
+            _.each(orm.collections, (model) => {
+              global[model.globalId || 'unknown'] = model;
+            });
+
+            return resolve();
           });
-
-          return resolve();
         });
-      });
+      }
+    } catch (ex) {
+      logService.warn('No waterline models detected');
     }
   },
 };
